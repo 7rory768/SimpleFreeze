3666a44cc9d2c2cfba638af535b1139c9f2c8856
7rory768 <roryskipper123@gmail.com>
2016-11-22 23:48:25 -0700

    Initial setup

--- src/org/plugins/simplefreeze/listeners/PlayerInteractListener.java
+++ src/org/plugins/simplefreeze/listeners/PlayerInteractListener.java
@@ -2,23 +2,29 @@ package org.plugins.simplefreeze.listeners;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.plugins.simplefreeze.SimpleFreezeMain;
 import org.plugins.simplefreeze.managers.PlayerManager;
 
 public class PlayerInteractListener implements Listener {
 
-	private final SimpleFreezeMain plugin;
-	private final PlayerManager playerManager;
-	
-	public PlayerInteractListener(SimpleFreezeMain plugin, PlayerManager playerManager) {
-		this.plugin = plugin;
-		this.playerManager = playerManager;
-	}
-	
-	@EventHandler
-	public void onInteract(PlayerInteractEvent e) {
-		
-	}
+    private final SimpleFreezeMain plugin;
+    private final PlayerManager playerManager;
+
+    public PlayerInteractListener(SimpleFreezeMain plugin, PlayerManager playerManager) {
+        this.plugin = plugin;
+        this.playerManager = playerManager;
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e) {
+        if (this.playerManager.isFrozen(e.getPlayer()) && (e.getAction() == Action.LEFT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_BLOCK)) {
+            e.setCancelled(true);
+            for (String msg : this.plugin.getConfig().getStringList("interact-message")) {
+                e.getPlayer().sendMessage(this.plugin.placeholders(msg));
+            }
+        }
+    }
 
 }
