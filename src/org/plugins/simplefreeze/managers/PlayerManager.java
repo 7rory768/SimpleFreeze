e5fc291f3f62e7d775c7823f3a9d4c6fd8838365
7rory768 <roryskipper123@gmail.com>
2016-11-22 23:49:14 -0700

    Added additional methods that use Player parameters instead of UUIDs

--- src/org/plugins/simplefreeze/managers/PlayerManager.java
+++ src/org/plugins/simplefreeze/managers/PlayerManager.java
@@ -1,47 +1,55 @@
 package org.plugins.simplefreeze.managers;
 
-import java.util.HashMap;
-import java.util.UUID;
-
 import org.bukkit.entity.Player;
 import org.plugins.simplefreeze.objects.FrozenPlayer;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class PlayerManager {
 
 //	private final SimpleFreezeMain plugin;
-	
-	private HashMap<UUID, FrozenPlayer> frozenPlayers = new HashMap<UUID, FrozenPlayer>();
-	
+
+    private HashMap<UUID, FrozenPlayer> frozenPlayers = new HashMap<UUID, FrozenPlayer>();
+
 //	public PlayerManager(SimpleFreezeMain plugin) {
 //		this.plugin = plugin;
 //	}
-	
-	public HashMap<UUID, FrozenPlayer> getFrozenPlayers() {
-		return this.frozenPlayers;
-	}
-	
-	public void addFrozenPlayer(UUID uuid, FrozenPlayer frozenPlayer) {
-		this.frozenPlayers.put(uuid, frozenPlayer);	
-	}
-	
-	public void removeFrozenPlayer(UUID uuid) {
-		this.frozenPlayers.remove(uuid);
-	}
-	
-	public boolean isFrozen(Player p) {
-		return this.frozenPlayers.containsKey(p.getUniqueId());
-	}
-	
-	public boolean isFrozen(UUID uuid) {
-		return this.frozenPlayers.containsKey(uuid);
-	}
-	
-	public boolean isSQLFrozen(UUID uuid) {
-		return this.frozenPlayers.containsKey(uuid) ? this.frozenPlayers.get(uuid).isSqlFreeze() : false;
-	}
-	
-	public FrozenPlayer getFrozenPlayer(UUID uuid) {
-		return this.isFrozen(uuid) ? this.frozenPlayers.get(uuid) : null;
-	}
-	
+
+    public HashMap<UUID, FrozenPlayer> getFrozenPlayers() {
+        return this.frozenPlayers;
+    }
+
+    public void addFrozenPlayer(UUID uuid, FrozenPlayer frozenPlayer) {
+        this.frozenPlayers.put(uuid, frozenPlayer);
+    }
+
+    public void removeFrozenPlayer(Player p) {
+        this.frozenPlayers.remove(p.getUniqueId());
+    }
+
+    public void removeFrozenPlayer(UUID uuid) {
+        this.frozenPlayers.remove(uuid);
+    }
+
+    public boolean isFrozen(Player p) {
+        return this.isFrozen(p.getUniqueId());
+    }
+
+    public boolean isFrozen(UUID uuid) {
+        return this.frozenPlayers.containsKey(uuid);
+    }
+
+    public boolean isSQLFrozen(UUID uuid) {
+        return this.frozenPlayers.containsKey(uuid) ? this.frozenPlayers.get(uuid).isSqlFreeze() : false;
+    }
+
+    public FrozenPlayer getFrozenPlayer(Player p) {
+        return this.getFrozenPlayer(p.getUniqueId());
+    }
+
+    public FrozenPlayer getFrozenPlayer(UUID uuid) {
+        return this.isFrozen(uuid) ? this.frozenPlayers.get(uuid) : null;
+    }
+
 }
