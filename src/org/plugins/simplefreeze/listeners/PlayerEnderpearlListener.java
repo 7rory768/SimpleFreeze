8bdead48aa0f2755aa2e7c4cc54ad6062e7dc6e8
7rory768 <roryskipper123@gmail.com>
2016-11-22 23:47:54 -0700

    Initial setup

--- src/org/plugins/simplefreeze/listeners/PlayerEnderpearlListener.java
+++ src/org/plugins/simplefreeze/listeners/PlayerEnderpearlListener.java
@@ -1,8 +1,10 @@
 package org.plugins.simplefreeze.listeners;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.ItemStack;
 import org.plugins.simplefreeze.SimpleFreezeMain;
 import org.plugins.simplefreeze.managers.PlayerManager;
 
@@ -18,6 +20,13 @@ public class PlayerEnderpearlListener implements Listener {
 	
 	@EventHandler
 	public void onEnderpearl(PlayerTeleportEvent e) {
+		if (e.getCause() == PlayerTeleportEvent.TeleportCause.ENDER_PEARL && this.playerManager.isFrozen(e.getPlayer())) {
+			e.setCancelled(true);
+			e.getPlayer().getInventory().addItem(new ItemStack(Material.ENDER_PEARL));
+			for (String msg : this.plugin.getConfig().getStringList("enderpearl-message")) {
+				e.getPlayer().sendMessage(this.plugin.placeholders(msg));
+			}
+		}
 		
 	}
 
