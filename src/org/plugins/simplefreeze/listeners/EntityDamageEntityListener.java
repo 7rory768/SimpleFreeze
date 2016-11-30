dbdd4d62a8658f7963350ba5e4ee99ee43c25fb2
7rory768 <roryskipper123@gmail.com>
2016-11-29 21:37:54 -0700

    Fixed bug where frozen players could damage other players with projectiles

--- src/org/plugins/simplefreeze/listeners/EntityDamageEntityListener.java
+++ src/org/plugins/simplefreeze/listeners/EntityDamageEntityListener.java
@@ -1,6 +1,8 @@
 package org.plugins.simplefreeze.listeners;
 
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -34,6 +36,22 @@ public class EntityDamageEntityListener implements Listener {
 					for (String msg : this.plugin.getConfig().getStringList("frozen-attacked-message")) {
 						if (!msg.equals("")) {
 							p.sendMessage(this.plugin.placeholders(msg.replace("{PLAYER}", ((Player) e.getEntity()).getName())));
+						}
+					}
+				}
+			} else if (e.getEntity() instanceof Player && e.getDamager() instanceof Projectile) {
+				Projectile projectile = (Projectile) e.getDamager();
+				LivingEntity livingEntity = projectile.getShooter();
+				if (livingEntity != null) {
+					if (livingEntity instanceof Player) {
+						Player p = (Player) livingEntity;
+						if (this.playerManager.isFrozen(p)) {
+							e.setCancelled(true);
+							for (String msg : this.plugin.getConfig().getStringList("frozen-attack-message")) {
+								if (!msg.equals("")) {
+									p.sendMessage(this.plugin.placeholders(msg));
+								}
+							}
 						}
 					}
 				}
