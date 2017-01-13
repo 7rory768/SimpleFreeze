7a4c6c9cd28a4cbe6ce6b5c9f62cc88c9c1df688
Rory <roryskipper123@gmail.com>
2017-01-12 22:56:10 -0700

    Setup to update /frozen formats on /sf reload

--- src/org/plugins/simplefreeze/commands/SimpleFreezeCommand.java
+++ src/org/plugins/simplefreeze/commands/SimpleFreezeCommand.java
@@ -7,10 +7,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.plugins.simplefreeze.SimpleFreezeMain;
+import org.plugins.simplefreeze.cache.FrozenPages;
 import org.plugins.simplefreeze.managers.FreezeManager;
 import org.plugins.simplefreeze.managers.HelmetManager;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class SimpleFreezeCommand implements CommandExecutor {
@@ -18,11 +20,13 @@ public class SimpleFreezeCommand implements CommandExecutor {
     private final SimpleFreezeMain plugin;
     private final FreezeManager freezeManager;
     private final HelmetManager helmetManager;
+    private final FrozenPages frozenPages;
 
-    public SimpleFreezeCommand(SimpleFreezeMain plugin, FreezeManager freezeManager, HelmetManager helmetManager) {
+    public SimpleFreezeCommand(SimpleFreezeMain plugin, FreezeManager freezeManager, HelmetManager helmetManager, FrozenPages frozenPages) {
         this.plugin = plugin;
         this.freezeManager = freezeManager;
         this.helmetManager = helmetManager;
+        this.frozenPages = frozenPages;
     }
 
     @Override
@@ -34,14 +38,77 @@ public class SimpleFreezeCommand implements CommandExecutor {
 
                 if (args[0].equalsIgnoreCase("reload")) {
                     if (!sender.hasPermission("sf.reload")) {
-                        sender.sendMessage(this.plugin.placeholders(this.plugin.getConfig().getString("no-permission-message")));
+                        for (String msg : this.plugin.getConfig().getStringList("no-permission-message")) {
+                            if (!msg.equals("")) {
+                                sender.sendMessage(this.plugin.placeholders(msg));
+                            }
+                        }
                         return false;
+                    }
+
+                    // STORE OLD /FROZEN FORMATS
+                    String frozenStr = this.plugin.getConfig().getString("frozen-list-format.formats.frozen");
+                    String frozenLocStr = this.plugin.getConfig().getString("frozen-list-format.formats.frozen-location");
+                    String tempFrozenStr = this.plugin.getConfig().getString("frozen-list-format.formats.temp-frozen");
+                    String tempFrozenLocStr = this.plugin.getConfig().getString("frozen-list-format.formats.temp-frozen-location");
+                    String onlinePlaceholder = this.plugin.getConfig().getString("frozen-list-format.online-placeholder");
+                    String offlinePlaceholder = this.plugin.getConfig().getString("frozen-list-format.offline-placeholder");
+
+                    this.plugin.reloadConfig();
+                    this.plugin.updateFinalPrefixFormatting();
+
+                    HashSet<String> strings = new HashSet<String>();
+
+                    // CHECK IF /FROZEN FORMATS CHANGED
+                    if (!frozenStr.equals(this.plugin.getConfig().getString("frozen-list-format.formats.frozen"))) {
+                        strings.add("frozen");
+                    }
+                    if (!frozenLocStr.equals(this.plugin.getConfig().getString("frozen-list-format.formats.frozen-location"))) {
+                        strings.add("frozen-location");
+                    }
+                    if (!tempFrozenStr.equals(this.plugin.getConfig().getString("frozen-list-format.formats.temp-frozen"))) {
+                        strings.add("temp-frozen");
+                    }
+                    if (!tempFrozenLocStr.equals(this.plugin.getConfig().getString("frozen-list-format.formats.temp-frozen-location"))) {
+                        strings.add("temp-frozen-location");
+                    }
+
+                    if (!onlinePlaceholder.equals(this.plugin.getConfig().getString("frozen-list-format.online-placeholder"))) {
+                        if (this.plugin.getConfig().getString("frozen-list-format.formats.frozen").contains("{ONLINE}")) {
+                            strings.add("frozen");
+                        }
+                        if (this.plugin.getConfig().getString("frozen-list-format.formats.frozen-location").contains("{ONLINE}")) {
+                            strings.add("frozen-location");
+                        }
+                        if (this.plugin.getConfig().getString("frozen-list-format.formats.temp-frozen").contains("{ONLINE}")) {
+                            strings.add("temp-frozen");
+                        }
+                        if (this.plugin.getConfig().getString("frozen-list-format.formats.temp-frozen-location").contains("{ONLINE}")) {
+                            strings.add("temp-frozen-location");
+                        }
+                    }
+
+                    if (!offlinePlaceholder.equals(this.plugin.getConfig().getString("frozen-list-format.offline-placeholder"))) {
+                        if (this.plugin.getConfig().getString("frozen-list-format.formats.frozen").contains("{OFFLINE}")) {
+                            strings.add("frozen");
+                        }
+                        if (this.plugin.getConfig().getString("frozen-list-format.formats.frozen-location").contains("{OFFLINE}")) {
+                            strings.add("frozen-location");
+                        }
+                        if (this.plugin.getConfig().getString("frozen-list-format.formats.temp-frozen").contains("{OFFLINE}")) {
+                            strings.add("temp-frozen");
+                        }
+                        if (this.plugin.getConfig().getString("frozen-list-format.formats.temp-frozen-location").contains("{OFFLINE}")) {
+                            strings.add("temp-frozen-location");
+                        }
+                    }
+
+                    if (!strings.isEmpty()) {
+                        this.frozenPages.refreshStrings(strings);
                     }
 
                     // REPLACE HELMETS IF THEY CHANGED,
                     // CHANGE PARTICLES IF DIFFERENT
-                    this.plugin.reloadConfig();
-                    this.plugin.updateFinalPrefixFormatting();
                     ItemStack newHelmetItem = null;
                     if (this.plugin.getConfig().isSet("head-item")) {
                         short data = this.plugin.getConfig().isSet("head-item.data") ? (short) this.plugin.getConfig().getInt("head-item.data") : 0;
