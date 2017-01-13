f7702a6fe2b987deab909e09e3b44187175804d2
Rory <roryskipper123@gmail.com>
2017-01-12 23:03:22 -0700

    Setup to cancel TempFrozenPlayer tasks

--- src/org/plugins/simplefreeze/listeners/PlayerQuitListener.java
+++ src/org/plugins/simplefreeze/listeners/PlayerQuitListener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.plugins.simplefreeze.SimpleFreezeMain;
 import org.plugins.simplefreeze.managers.PlayerManager;
+import org.plugins.simplefreeze.objects.TempFrozenPlayer;
 
 public class PlayerQuitListener implements Listener {
 
@@ -20,6 +21,14 @@ public class PlayerQuitListener implements Listener {
 	public void onQuit(PlayerQuitEvent e) {
 		if (this.playerManager.isFrozen(e.getPlayer())) {
 			e.getPlayer().getInventory().setHelmet(this.playerManager.getFrozenPlayer(e.getPlayer()).getHelmet());
+			if (this.plugin.getConfig().getBoolean("enable-fly")) {
+				e.getPlayer().setAllowFlight(false);
+				e.getPlayer().setFlying(false);
+			}
+			if (this.playerManager.getFrozenPlayer(e.getPlayer()) instanceof TempFrozenPlayer) {
+				((TempFrozenPlayer) this.playerManager.getFrozenPlayer(e.getPlayer())).cancelTask();
+			}
+			this.playerManager.removeFrozenPlayer(e.getPlayer());
 		}
 	}
 
