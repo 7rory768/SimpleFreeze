544f709f0405aa8fab13a1b1b4c26650222eae43
Rory <roryskipper123@gmail.com>
2017-01-12 23:05:38 -0700

    Setup to update FrozenPages

--- src/org/plugins/simplefreeze/managers/PlayerManager.java
+++ src/org/plugins/simplefreeze/managers/PlayerManager.java
@@ -1,6 +1,9 @@
 package org.plugins.simplefreeze.managers;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.plugins.simplefreeze.SimpleFreezeMain;
+import org.plugins.simplefreeze.cache.FrozenPages;
 import org.plugins.simplefreeze.objects.FrozenPlayer;
 
 import java.util.HashMap;
@@ -8,13 +11,15 @@ import java.util.UUID;
 
 public class PlayerManager {
 
-//	private final SimpleFreezeMain plugin;
+	private final SimpleFreezeMain plugin;
+	private final FrozenPages frozenPages;
 
     private HashMap<UUID, FrozenPlayer> frozenPlayers = new HashMap<UUID, FrozenPlayer>();
 
-//	public PlayerManager(SimpleFreezeMain plugin) {
-//		this.plugin = plugin;
-//	}
+	public PlayerManager(SimpleFreezeMain plugin, FrozenPages frozenPages) {
+		this.plugin = plugin;
+		this.frozenPages = frozenPages;
+	}
 
     public HashMap<UUID, FrozenPlayer> getFrozenPlayers() {
         return this.frozenPlayers;
@@ -22,14 +27,17 @@ public class PlayerManager {
 
     public void addFrozenPlayer(UUID uuid, FrozenPlayer frozenPlayer) {
         this.frozenPlayers.put(uuid, frozenPlayer);
+        this.frozenPages.refreshString(uuid);
+        Bukkit.broadcastMessage("refreshing");
     }
 
     public void removeFrozenPlayer(Player p) {
-        this.frozenPlayers.remove(p.getUniqueId());
+        this.removeFrozenPlayer(p.getUniqueId());
     }
 
     public void removeFrozenPlayer(UUID uuid) {
         this.frozenPlayers.remove(uuid);
+        this.frozenPages.removePlayer(uuid);
     }
 
     public boolean isFrozen(Player p) {
@@ -37,11 +45,21 @@ public class PlayerManager {
     }
 
     public boolean isFrozen(UUID uuid) {
-        return this.frozenPlayers.containsKey(uuid);
+       if (this.frozenPlayers.containsKey(uuid)) {
+           return true;
+       } else if (this.plugin.getPlayerConfig().getConfig().isSet("players." + uuid.toString())) {
+           if (this.plugin.getPlayerConfig().getConfig().isSet("players." + uuid.toString() + ".unfreeze-date")) {
+                if (System.currentTimeMillis() > this.plugin.getPlayerConfig().getConfig().getLong("players." + uuid.toString() + ".unfreeze-date")) {
+                    return false;
+                }
+           }
+           return true;
+       }
+       return false;
     }
 
     public boolean isSQLFrozen(UUID uuid) {
-        return this.frozenPlayers.containsKey(uuid) ? this.frozenPlayers.get(uuid).isSqlFreeze() : false;
+        return this.frozenPlayers.containsKey(uuid) ? this.frozenPlayers.get(uuid).isSqlFreeze() : this.plugin.getPlayerConfig().getConfig().getBoolean("players." + uuid.toString() + ".mysql", false);
     }
 
     public FrozenPlayer getFrozenPlayer(Player p) {
