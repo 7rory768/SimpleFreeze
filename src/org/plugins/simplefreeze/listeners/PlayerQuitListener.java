5fb7fc9b2447d780ccf652cf4d1cfbd4968faaa4
Rory <roryskipper123@gmail.com>
2017-01-15 12:56:59 -0700

    Fixed a bug where players that were unfrozen while offline wouldn't return to their original location

--- src/org/plugins/simplefreeze/listeners/PlayerQuitListener.java
+++ src/org/plugins/simplefreeze/listeners/PlayerQuitListener.java
@@ -1,5 +1,6 @@
 package org.plugins.simplefreeze.listeners;
 
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -9,27 +10,31 @@ import org.plugins.simplefreeze.objects.TempFrozenPlayer;
 
 public class PlayerQuitListener implements Listener {
 
-	private final SimpleFreezeMain plugin;
-	private final PlayerManager playerManager;
-	
-	public PlayerQuitListener(SimpleFreezeMain plugin, PlayerManager playerManager) {
-		this.plugin = plugin;
-		this.playerManager = playerManager;
-	}
-	
-	@EventHandler
-	public void onQuit(PlayerQuitEvent e) {
-		if (this.playerManager.isFrozen(e.getPlayer())) {
-			e.getPlayer().getInventory().setHelmet(this.playerManager.getFrozenPlayer(e.getPlayer()).getHelmet());
-			if (this.plugin.getConfig().getBoolean("enable-fly")) {
-				e.getPlayer().setAllowFlight(false);
-				e.getPlayer().setFlying(false);
-			}
-			if (this.playerManager.getFrozenPlayer(e.getPlayer()) instanceof TempFrozenPlayer) {
-				((TempFrozenPlayer) this.playerManager.getFrozenPlayer(e.getPlayer())).cancelTask();
-			}
-			this.playerManager.removeFrozenPlayer(e.getPlayer());
-		}
-	}
+    private final SimpleFreezeMain plugin;
+    private final PlayerManager playerManager;
+
+    public PlayerQuitListener(SimpleFreezeMain plugin, PlayerManager playerManager) {
+        this.plugin = plugin;
+        this.playerManager = playerManager;
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        if (this.playerManager.isFrozen(e.getPlayer())) {
+            Location originalLocation = this.playerManager.getOriginalLocation(e.getPlayer().getUniqueId());
+            if (originalLocation != null) {
+                e.getPlayer().teleport(originalLocation);
+            }
+            e.getPlayer().getInventory().setHelmet(this.playerManager.getFrozenPlayer(e.getPlayer()).getHelmet());
+            if (this.plugin.getConfig().getBoolean("enable-fly")) {
+                e.getPlayer().setAllowFlight(false);
+                e.getPlayer().setFlying(false);
+            }
+            if (this.playerManager.getFrozenPlayer(e.getPlayer()) instanceof TempFrozenPlayer) {
+                ((TempFrozenPlayer) this.playerManager.getFrozenPlayer(e.getPlayer())).cancelTask();
+            }
+            this.playerManager.removeFrozenPlayer(e.getPlayer());
+        }
+    }
 
 }
