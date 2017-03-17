7e627c46d4e865a5ea13fe34c6c07d523479562a
Rory <roryskipper123@gmail.com>
2017-03-16 19:27:13 -0600

    Added unfreeze on ban

--- src/org/plugins/simplefreeze/hooks/BanManagerHook.java
+++ src/org/plugins/simplefreeze/hooks/BanManagerHook.java
@@ -1,5 +1,68 @@
 package org.plugins.simplefreeze.hooks;
 
-public class BanManagerHook {
+import me.confuser.banmanager.BmAPI;
+import me.confuser.banmanager.events.IpBanEvent;
+import me.confuser.banmanager.events.NameBannedEvent;
+import me.confuser.banmanager.events.PlayerBannedEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.plugins.simplefreeze.SimpleFreezeMain;
+import org.plugins.simplefreeze.managers.FreezeManager;
+import org.plugins.simplefreeze.managers.PlayerManager;
 
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class BanManagerHook implements Listener {
+
+    private final SimpleFreezeMain plugin;
+    private final PlayerManager playerManager;
+    private final FreezeManager freezeManager;
+
+    public BanManagerHook(SimpleFreezeMain plugin, PlayerManager playerManager, FreezeManager freezeManager) {
+        this.plugin = plugin;
+        this.playerManager = playerManager;
+        this.freezeManager = freezeManager;
+    }
+
+    @EventHandler
+    public void onPlayerBanned(PlayerBannedEvent e) {
+        if (this.plugin.usingBanManager()) {
+            UUID uuid = e.getBan().getPlayer().getUUID();
+            if (this.playerManager.isFrozen(uuid) && !this.playerManager.isFreezeAllFrozen(uuid)) {
+                this.freezeManager.unfreeze(uuid);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onNameBanned(NameBannedEvent e) {
+        if (this.plugin.usingBanManager()) {
+            UUID uuid = Bukkit.getOfflinePlayer(e.getBan().getName()).getUniqueId();
+            if (this.playerManager.isFrozen(uuid) && !this.playerManager.isFreezeAllFrozen(uuid)) {
+                this.freezeManager.unfreeze(uuid);
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onIPBan(IpBanEvent e) {
+        if (this.plugin.usingBanManager()) {
+            if (!e.isCancelled()) {
+                long ip = e.getBan().getIp();
+                for (Player p : Bukkit.getOnlinePlayers()) {
+                    try {
+                        if (this.playerManager.isFrozen(p) && !this.playerManager.isFreezeAllFrozen(p) && BmAPI.getPlayer(p).getIp() == ip) {
+                            this.freezeManager.unfreeze(p.getUniqueId());
+                        }
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 }
--- src/org/plugins/simplefreeze/hooks/EssentialsHook.java
+++ src/org/plugins/simplefreeze/hooks/EssentialsHook.java
@@ -1,5 +1,174 @@
 package org.plugins.simplefreeze.hooks;
 
-public class EssentialsHook {
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.plugins.simplefreeze.SimpleFreezeMain;
+import org.plugins.simplefreeze.managers.FreezeManager;
+import org.plugins.simplefreeze.managers.PlayerManager;
+
+import java.io.FileReader;
+import java.util.UUID;
+
+public class EssentialsHook implements Listener {
+
+    private final SimpleFreezeMain plugin;
+    private final PlayerManager playerManager;
+    private final FreezeManager freezeManager;
+
+    public EssentialsHook(SimpleFreezeMain plugin, PlayerManager playerManager, FreezeManager freezeManager) {
+        this.plugin = plugin;
+        this.playerManager = playerManager;
+        this.freezeManager = freezeManager;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onBan(PlayerCommandPreprocessEvent e) {
+        if (this.plugin.usingEssentials() && !e.isCancelled()) {
+            String cmd = e.getMessage();
+            String[] args = cmd.split("\\s+");
+            if (args.length > 1) {
+                final String arg2 = args[1];
+
+                UUID uuid = null;
+                Player p = Bukkit.getPlayer(arg2);
+                OfflinePlayer offlineP = Bukkit.getOfflinePlayer(arg2);
+
+                if (p != null) {
+                    uuid = p.getUniqueId();
+                } else if (offlineP != null) {
+                    if (offlineP.hasPlayedBefore()) {
+                        uuid = offlineP.getUniqueId();
+                    }
+                }
+
+                if (uuid != null) {
+                    if (args[0].equalsIgnoreCase("/ban")) {
+                        final UUID finalUUID = uuid;
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                if (isBanned(finalUUID) && playerManager.isFrozen(finalUUID) && !playerManager.isFreezeAllFrozen(finalUUID)) {
+                                    freezeManager.unfreeze(finalUUID);
+                                }
+                            }
+                        }.runTaskLater(this.plugin, 1L);
+                    } else if (args[0].equalsIgnoreCase("/tempban")) {
+                        final UUID finalUUID = uuid;
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                if (isBanned(finalUUID) && playerManager.isFrozen(finalUUID) && !playerManager.isFreezeAllFrozen(finalUUID)) {
+                                    freezeManager.unfreeze(finalUUID);
+                                }
+                            }
+                        }.runTaskLater(this.plugin, 1L);
+                    }
+                }
+
+/*                if (args[0].equalsIgnoreCase("/banip")) {
+                    List<UUID> potentionalPlayers = new ArrayList<UUID>();
+                    for (Player p1 : Bukkit.getOnlinePlayers()) {
+                        if (p1.getAddress().getHostName().equals(arg2)) {
+                            potentionalPlayers.add(p1.getUniqueId());
+                        }
+                    }
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            if (isBanned(arg2)) {
+                                for (UUID uuid : potentionalPlayers) {
+                                    if (playerManager.isFrozen(uuid) && !playerManager.isFreezeAllFrozen(uuid)) {
+                                        freezeManager.unfreeze(uuid);
+                                    }
+                                }
+                            }
+
+
+                        }
+                    }.runTaskLater(this.plugin, 1L);
+                }*/
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent e) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (isBanned(e.getPlayer().getAddress().getHostName())) {
+                    UUID uuid = e.getPlayer().getUniqueId();
+                    if (playerManager.isFrozen(uuid) && !playerManager.isFreezeAllFrozen(uuid)) {
+                        freezeManager.unfreeze(uuid);
+                    }
+                }
+
+
+            }
+        }.runTaskLater(this.plugin, 1L);
+    }
+
+    @EventHandler
+    public void onPlayerKick(PlayerKickEvent e) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (isBanned(e.getPlayer().getAddress().getHostName())) {
+                    UUID uuid = e.getPlayer().getUniqueId();
+                    if (playerManager.isFrozen(uuid) && !playerManager.isFreezeAllFrozen(uuid)) {
+                        freezeManager.unfreeze(uuid);
+                    }
+                }
+
+
+            }
+        }.runTaskLater(this.plugin, 1L);
+    }
+
+    private boolean isBanned(UUID uuid) {
+        JSONParser parser = new JSONParser();
+        String uuidStr = uuid.toString();
+        try {
+            Object parsedObj = parser.parse(new FileReader("banned-players.json"));
+            JSONArray list = (JSONArray) parsedObj;
+            for (Object obj : list) {
+                JSONObject jsonObj = (JSONObject) obj;
+                if (((String) jsonObj.get("uuid")).equalsIgnoreCase(uuidStr)) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private boolean isBanned(String ip) {
+        JSONParser parser = new JSONParser();
+        try {
+            Object parsedObj = parser.parse(new FileReader("banned-ips.json"));
+            JSONArray list = (JSONArray) parsedObj;
+            for (Object obj : list) {
+                JSONObject jsonObj = (JSONObject) obj;
+                if (((String) jsonObj.get("ip")).equalsIgnoreCase(ip)) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
 }
